bd5c3315cff80541392b4197267b39a7d3c13416
Clement Nicolas <clement.nicolas-ext@amundi.com>
2017-10-04 16:49:48 +0200

    test the perceptron

--- perceptron/src/test/kotlin/perceptron/PerceptronSimpleTest.kt
+++ perceptron/src/test/kotlin/perceptron/PerceptronSimpleTest.kt
@@ -6,7 +6,7 @@ import java.util.*
 
 class PerceptronSimpleTest {
 
-    private fun f(x: Double) = x
+    private fun h(a: Double, b: Double, c: Double) = a + b - c
 
     @Test
     fun weights_should_be_different_from_0() {
@@ -14,10 +14,12 @@ class PerceptronSimpleTest {
 
         val r = Random()
         val trainingData: Array<Trainer> = Array(2000) {
-            val x = r.nextDouble() * 100 * (if (r.nextBoolean()) 1 else -1)
-            val y = r.nextDouble() * 100 * (if (r.nextBoolean()) 1 else -1)
-            val answer = if (y < f(x)) LearningClasse.BAD else LearningClasse.GOOD
-            Trainer(doubleArrayOf(x, y), answer)
+            val a = r.nextDouble() * 100 * (if (r.nextBoolean()) 1 else -1)
+            val b = r.nextDouble() * 100 * (if (r.nextBoolean()) 1 else -1)
+            val c = r.nextDouble() * 100 * (if (r.nextBoolean()) 1 else -1)
+            val d = r.nextDouble() * 100 * (if (r.nextBoolean()) 1 else -1)
+            val answer = if (d < h(a, b, c)) LearningClasse.BAD else LearningClasse.GOOD
+            Trainer(doubleArrayOf(a, b, c, d), answer)
         }
 
         val weights = perceptron.run(trainingData, 1000)
@@ -26,8 +28,5 @@ class PerceptronSimpleTest {
         assertThat(weights).doesNotHaveDuplicates()
 
         println(Arrays.toString(weights))
-
-        assertThat(perceptron.decide(doubleArrayOf(10.0, 20.0)).classe).isEqualTo(LearningClasse.GOOD)
-        assertThat(perceptron.decide(doubleArrayOf(10.0, 1.0)).classe).isEqualTo(LearningClasse.BAD)
     }
 }
